f4c8285f20e2b10240f2b2ea19aa3859629c1ba5
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-06-26 10:54:05 -0400

    Fixed deployment error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
+allowed_origins = [
+  "http://localhost:5173",
+  "https://myglassstyle.com"
+]
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # or your domain later
+    origins(*allowed_origins)
     resource "*",
       headers: :any,
       methods: [:get, :post, :options]
