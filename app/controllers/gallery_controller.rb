14b3c5b8df45285138948db0a556be797308eafa
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-06-29 15:14:35 -0400

    Ready for Render deploy

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,22 +2,36 @@ class GalleryController < ApplicationController
   def show
     folder = params[:folder].capitalize # e.g., Showers, Mirrors
 
-    resources = Cloudinary::Api.resources(
+    # Fetch images from Cloudinary
+    images = Cloudinary::Api.resources(
       type: "upload",
+      resource_type: "image",
       prefix: "#{folder}/",
       max_results: 100
-    )
+    )["resources"]
+
+    # Fetch videos from Cloudinary
+    videos = Cloudinary::Api.resources(
+      type: "upload",
+      resource_type: "video",
+      prefix: "#{folder}/",
+      max_results: 100
+    )["resources"]
+
+    # Combine images and videos
+    resources = images + videos
 
     # Set caching for this API response (clients & proxies)
     expires_in 1.year, public: true
 
-    render json: resources["resources"].map do |img|
+    render json: resources.map do |res|
       {
-        url: img["secure_url"],
-        public_id: img["public_id"],
-        format: img["format"],
-        width: img["width"],
-        height: img["height"]
+        url: res["secure_url"],
+        public_id: res["public_id"],
+        format: res["format"],
+        width: res["width"],
+        height: res["height"],
+        resource_type: res["resource_type"] # add resource_type to distinguish video/image
       }
     end
   rescue => e
