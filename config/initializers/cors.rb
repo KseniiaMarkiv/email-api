b66721f38873c8c893459b07cf79026d0b3e7fef
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-06-29 19:20:17 -0400

    Fixing CORS on deploy side

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,16 @@
-allowed_origins = [
-  "http://localhost:5173",
-  "https://myglassstyle.com"
-]
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "http://localhost:5173"  # Or whatever your dev port is
+      resource "*", headers: :any, methods: [:get, :post, :options]
+    end
+  end
+end
+
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(*allowed_origins)
+    origins "https://myglassstyle.com"
     resource "*",
       headers: :any,
       methods: [:get, :post, :options]
