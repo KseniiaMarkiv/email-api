ef8c914e7905522cac5eec380c671b17ef8104af
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-07-26 02:51:59 -0400

    Fixing deployment

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,8 +2,7 @@ class GalleryController < ApplicationController
   require "imagekitio"
 
   def show
-    folder = params[:folder]
-
+    folder    = params[:folder]
     cache_key = "imagekit_gallery_data_#{folder}"
 
     resources = Rails.cache.fetch(cache_key, expires_in: 1.hour) do
@@ -13,17 +12,16 @@ class GalleryController < ApplicationController
         ENV["IMAGEKIT_URL_ENDPOINT"]
       )
 
-      list = imagekit.list_files({
+      list = imagekit.list_files(
         folder: "/#{folder}",
-        sort: "ASC_CREATED",
-        limit: 100
-      })
+        sort:   "ASC_CREATED",
+        limit:  100
+      )
 
       list[:response] || []
     end
 
     expires_in 1.year, public: true
-
     render json: resources.map { |res| format_resource(res) }
   rescue => e
     Rails.logger.error "GalleryController#show error: #{e.message}"
@@ -34,13 +32,13 @@ class GalleryController < ApplicationController
 
   def format_resource(res)
     {
-      url: res["url"],
-      file_id: res["fileId"],
+      url:       res["url"],
+      file_id:   res["fileId"],
       file_type: res["fileType"],
-      format: res["fileType"],
-      width: res["width"],
-      height: res["height"],
-      name: res["name"]
+      format:    res["fileType"],
+      width:     res["width"],
+      height:    res["height"],
+      name:      res["name"]
     }
   end
 end
